2417fc59a9161c72704dc44212820e3233d6a7d2
ikexing-cn <75018198+ikexing-cn@users.noreply.github.com>
2021-03-05 16:58:14 +0800

    存git

---
+++ src/main/java/ikexing/atutils/ATUtils.java
@@ -0,0 +1,31 @@
+package ikexing.atutils;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ATUtils.MODID, name = ATUtils.NAME, version = ATUtils.VERSION)
+public class ATUtils
+{
+    public static final String MODID = "atutils";
+    public static final String NAME = "AutoTech Utils";
+    public static final String VERSION = "1.0.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
