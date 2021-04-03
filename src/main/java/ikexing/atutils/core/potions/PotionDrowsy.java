5a40a4cf54fab1dd5fe17b6cf62d69e773eeb869
ikexing-cn <75018198+ikexing-cn@users.noreply.github.com>
2021-04-03 20:47:08 +0800

    new potions : drowsy

--- src/main/java/ikexing/atutils/core/potions/PotionDrowsy.java
+++ src/main/java/ikexing/atutils/core/potions/PotionDrowsy.java
@@ -1,9 +1,16 @@
 package ikexing.atutils.core.potions;
 
 import ikexing.atutils.ATUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.Objects;
 
 public class PotionDrowsy extends Potion {
+    private static final ResourceLocation TEXTURE =
+            new ResourceLocation(ATUtils.MODID + ":textures/gui/drowsy.png");
 
     public PotionDrowsy() {
         super(false, 0x6688FF);
@@ -11,5 +18,21 @@ public class PotionDrowsy extends Potion {
         this.setRegistryName(ATUtils.MODID + ":drowsy");
     }
 
-    
+    @Override
+    public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc) {
+        int duration = effect.getDuration();
+        int fIndex = (duration % 16) / 2;
+
+        mc.getTextureManager().bindTexture(TEXTURE);
+        Objects.requireNonNull(mc.currentScreen).drawTexturedModalRect(
+                x + 6, y + 7, fIndex * 18, 0, 18, 18);
+    }
+
+    @Override
+    public void renderHUDEffect(int x, int y, PotionEffect effect, Minecraft mc, float alpha) {
+        mc.getTextureManager().bindTexture(TEXTURE);
+        mc.ingameGUI.drawTexturedModalRect(
+                x + 3, y + 3, 0, 0, 18, 18);
+    }
+
 }
