0dc8f7ef4ce23d81cc325e0ea3d6332da6f25a06
niyan <2093615664@qq.com>
2021-04-07 14:09:15 +0800

    update Jei plugin

---
+++ src/main/java/ikexing/atutils/client/jei/JeiATPlugin.java
@@ -0,0 +1,13 @@
+package ikexing.atutils.client.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+
+public class JeiATPlugin implements IModPlugin {
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registry) {
+
+    }
+
+}
