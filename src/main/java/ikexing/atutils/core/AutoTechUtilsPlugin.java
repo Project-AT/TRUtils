0770ea17da916548030e29b7f3819665f7baa361
ikexing-cn <75018198+ikexing-cn@users.noreply.github.com>
2021-04-09 22:57:42 +0800

    mixin over

--- src/main/java/ikexing/atutils/core/AutoTechUtilsPlugin.java
+++ src/main/java/ikexing/atutils/core/AutoTechUtilsPlugin.java
@@ -1,31 +1,62 @@
 package ikexing.atutils.core;
 
-import net.minecraft.launchwrapper.Launch;
+import net.minecraftforge.fml.relauncher.CoreModManager;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import org.apache.commons.lang3.reflect.MethodUtils;
+import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
+import javax.annotation.Nullable;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.CodeSource;
 import java.util.Map;
 
 @IFMLLoadingPlugin.Name("atutilscore")
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 public class AutoTechUtilsPlugin implements IFMLLoadingPlugin {
-    public static void initMixin(){
+    public AutoTechUtilsPlugin() {
         MixinBootstrap.init();
         Mixins.addConfiguration("mixins.atutils.init.json");
-    }
-    @Override public void injectData(Map<String, Object> data){
-        try {
-            ClassLoader appClassLoader = Launch.class.getClassLoader();
-            MethodUtils.invokeMethod(appClassLoader, true, "addURL", this.getClass().getProtectionDomain().getCodeSource().getLocation());
-            MethodUtils.invokeStaticMethod(appClassLoader.loadClass(this.getClass().getName()), "initMixin");
-        } catch (Exception ignored) {}
+        CodeSource codeSource = this.getClass().getProtectionDomain().getCodeSource();
+        if (codeSource != null) {
+            URL location = codeSource.getLocation();
+            try {
+                File file = new File(location.toURI());
+                if (file.isFile()) {
+                    CoreModManager.getReparseableCoremods().remove(file.getName());
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            LogManager.getLogger().warn(this.getClass().getProtectionDomain());
+        }
     }
 
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
 
-    @Override public String[] getASMTransformerClass() { return null; }
-    @Override public String getModContainerClass() { return null; }
-    @Override public String getSetupClass() { return null; }
-    @Override public String getAccessTransformerClass() { return null; }
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
 }
