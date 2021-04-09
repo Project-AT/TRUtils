81271c7c3d16df037d5456e2b5f358d024d61958
niyan <2093615664@qq.com>
2021-04-09 18:21:35 +0800

    finish jei function. (doge)

--- src/main/java/ikexing/atutils/client/jei/hydroangeas/HydroangeasRecipeCategory.java
+++ src/main/java/ikexing/atutils/client/jei/hydroangeas/HydroangeasRecipeCategory.java
@@ -57,13 +57,13 @@ public class HydroangeasRecipeCategory implements IRecipeCategory<HydroangeasRec
         recipeLayout.getItemStacks().init(0, true, 12, 12);
         recipeLayout.getItemStacks().set(0, ItemBlockSpecialFlower.ofType(LibBlockNames.SUBTILE_HYDROANGEAS));
 
-        recipeLayout.getFluidStacks().init(1, true, 76, 12, 16, 16, 1000, false, null);
+        recipeLayout.getFluidStacks().init(1, true, 100, 12, 16, 16, 1000, false, null);
         recipeLayout.getFluidStacks().set(1, ingredients.getInputs(VanillaTypes.FLUID).get(0));
 
         recipeLayout.getItemStacks().init(2, true, 12, 36);
         recipeLayout.getItemStacks().set(2, ingredients.getInputs(VanillaTypes.ITEM).get(0));
 
-        recipeLayout.getFluidStacks().init(3, true, 76, 36, 16, 16, 1000, false, null);
+        recipeLayout.getFluidStacks().init(3, true, 100, 36, 16, 16, 1000, false, null);
         recipeLayout.getFluidStacks().set(3, ingredients.getInputs(VanillaTypes.FLUID).get(1));
 
     }
--- src/main/java/ikexing/atutils/client/jei/hydroangeas/HydroangeasRecipeWrapper.java
+++ src/main/java/ikexing/atutils/client/jei/hydroangeas/HydroangeasRecipeWrapper.java
@@ -6,9 +6,12 @@ import mezz.jei.api.ingredients.VanillaTypes;
 import mezz.jei.api.recipe.IRecipeWrapper;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.relauncher.Side;
@@ -20,8 +23,9 @@ import java.util.Map;
 
 public class HydroangeasRecipeWrapper implements IRecipeWrapper {
 
-    public final static String S_CONSUME = I18n.format("atutils.jei.hydroangeas.consume");
-    public final static String S_FACTOR = I18n.format("atutils.jei.hydroangeas.blockFactor");
+    public final static String S_CONSUME = new TextComponentTranslation("atutils.jei.hydroangeas.consume").getUnformattedComponentText();
+    public final static String S_FACTOR = new TextComponentTranslation("atutils.jei.hydroangeas.blockFactor").getUnformattedComponentText();
+    public final static int DARK_GREY = TextFormatting.DARK_GRAY.getColorIndex();
 
     private Block input;
     private Block blockBelow;
@@ -38,7 +42,13 @@ public class HydroangeasRecipeWrapper implements IRecipeWrapper {
     @SideOnly(Side.CLIENT)
     @Override
     public void drawInfo(Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
+        FontRenderer fontRenderer = minecraft.fontRenderer;
 
+        fontRenderer.drawString(S_CONSUME, 40, 12, DARK_GREY);
+        fontRenderer.drawString(S_FACTOR, 40, 36, DARK_GREY);
+
+        fontRenderer.drawString("Mana x" + fluidFactorHandler.getManaFactor(), 12, 54, DARK_GREY);
+        fontRenderer.drawString("Mana x" + manaFactorBlock, 100, 54, DARK_GREY);
     }
 
     @Override
