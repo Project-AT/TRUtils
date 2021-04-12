5563728ae1169f803c9c8bb36e8e567209f76e73
ikexing-cn <75018198+ikexing-cn@users.noreply.github.com>
2021-04-12 13:39:49 +0800

    fuck manacraft biome

--- src/main/java/ikexing/atutils/ATUtils.java
+++ src/main/java/ikexing/atutils/ATUtils.java
@@ -22,7 +22,7 @@ import static ikexing.atutils.core.config.ATConfig.HydroangeasModified;
 public class ATUtils {
     public static final String MODID = "atutils";
     public static final String NAME = "AutoTech Utils";
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
     public static final String dependencies = "required-after:crafttweaker;after:twilightforest;after:botania;before:mana_craft";
 
     public static Logger logger;
---
+++ src/main/java/ikexing/atutils/mixins/MixinBiomeMana.java
@@ -0,0 +1,20 @@
+package ikexing.atutils.mixins;
+
+import mana_craft.init.ManaCraftBiomes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = ManaCraftBiomes.class, remap = false)
+public class MixinBiomeMana {
+    @Inject(method = "init", at = @At("HEAD"), cancellable = true)
+    private static void InjectInit(CallbackInfo ci) {
+        ci.cancel();
+    }
+
+    @Inject(method = "preInit", at = @At("HEAD"), cancellable = true)
+    private static void InjectPreInit(CallbackInfo ci) {
+        ci.cancel();
+    }
+}
