b9be58e7020cbee56c5880515f79f39b6a0e0608
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-08-02 19:26:17 +0800

    Don't rely on that

--- src/main/java/ikexing/atutils/mixins/MixinBiomeMana.java
+++ src/main/java/ikexing/atutils/mixins/MixinBiomeMana.java
@@ -2,10 +2,12 @@ package ikexing.atutils.mixins;
 
 import mana_craft.init.ManaCraftBiomes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Pseudo
 @Mixin(value = ManaCraftBiomes.class, remap = false)
 public class MixinBiomeMana {
     @Inject(method = "init", at = @At("HEAD"), cancellable = true)
--- src/main/java/ikexing/atutils/mixins/MixinItemMPGenerator.java
+++ src/main/java/ikexing/atutils/mixins/MixinItemMPGenerator.java
@@ -5,11 +5,13 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vazkii.botania.common.block.ModBlocks;
 
+@Pseudo
 @Mixin(value = ItemMPGenerator.class, remap = false)
 public class MixinItemMPGenerator {
 
