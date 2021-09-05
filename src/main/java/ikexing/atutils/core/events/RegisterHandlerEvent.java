c2fea3a3c1c6f151dd2ef03e14c8cfb76a1a431c
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-09-05 17:29:46 +0800

    添加未激活的研究台

--- src/main/java/ikexing/atutils/core/events/RegisterHandlerEvent.java
+++ src/main/java/ikexing/atutils/core/events/RegisterHandlerEvent.java
@@ -4,12 +4,14 @@ import epicsquid.mysticallib.LibRegistry;
 import epicsquid.mysticallib.entity.RenderNull;
 import epicsquid.mysticallib.event.RegisterContentEvent;
 import ikexing.atutils.ATUtils;
+import ikexing.atutils.core.block.BlockNoEnergyTable;
 import ikexing.atutils.core.item.AuthorFood;
 import ikexing.atutils.core.item.AuthorFood.AuthorInformation;
 import ikexing.atutils.core.item.CrudeSteel;
 import ikexing.atutils.core.item.botania.AdvanceStickThunder;
 import ikexing.atutils.core.item.botania.StickThunder;
 import ikexing.atutils.core.ritual.entity.EntityRitualMagneticAttraction;
+import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
@@ -31,16 +33,25 @@ public class RegisterHandlerEvent {
         registry.register(StickThunder.INSTANCE);
         registry.register(ATUtils.magneticAttraction);
         registry.register(AdvanceStickThunder.INSTANCE);
+        registry.register(BlockNoEnergyTable.ITEM_BLOCK);
         registry.registerAll(CrudeSteel.ITEMS.toArray(new Item[0]));
         registry.registerAll(AuthorFood.AUTHOR_QQ_NUMBER.stream().map(AuthorInformation::of).peek(AuthorFood.ITEM_FOODS::add).toArray(ItemFood[]::new));
+    }
+
+    @SubscribeEvent
+    public static void onBlockRegistry(Register<Block> event) {
+        IForgeRegistry<Block> registry = event.getRegistry();
+        registry.register(BlockNoEnergyTable.INSTANCE);
     }
 
     @SubscribeEvent
     public static void onModelRegistry(ModelRegistryEvent event) {
         AuthorFood.convert();
         regModel(ATUtils.magneticAttraction);
+        regModel(BlockNoEnergyTable.ITEM_BLOCK);
         CrudeSteel.ITEMS.forEach(RegisterHandlerEvent::regModel);
         AuthorFood.ITEM_FOODS.forEach(RegisterHandlerEvent::regModel);
+        //ModelLoader.setCustomModelResourceLocation(BlockNoEnergyTable.ITEM_BLOCK, 0, new ModelResourceLocation(Objects.requireNonNull(BlockNoEnergyTable.ITEM_BLOCK.getRegistryName()), "facing=north"));
     }
 
     @SubscribeEvent
