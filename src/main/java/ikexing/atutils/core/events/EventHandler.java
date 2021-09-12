1f13673ec65ea7bf8f338db1558c8ca12c6e3233
superhelo <xiaohaluo@qq.com>
2021-09-12 22:03:12 +0800

    修改颜色为栗色

--- src/main/java/ikexing/atutils/core/events/EventHandler.java
+++ src/main/java/ikexing/atutils/core/events/EventHandler.java
@@ -32,7 +32,7 @@ public class EventHandler {
                             world.setBlockState(blockPos, blockState.withProperty(BlockEvilStone.STATUS, status + 1));
                         } else {
                             for(int i = 0; i < 5; i++) {
-                                Botania.proxy.wispFX(blockPos.getX() + Math.random() * 1.25D, blockPos.getY() * Math.random() * 1.5D, blockPos.getZ() + Math.random() * 1.25D,  (float) (Math.random() *  Math.random()),  (float) (Math.random() * Math.random()),  (float) (Math.random() * Math.random()), (float) Math.random() * 2.0F);
+                                Botania.proxy.wispFX(blockPos.getX() + Math.random() * 1.25D, blockPos.getY() * Math.random() * 1.5D, blockPos.getZ() + Math.random() * 1.25D,  0.5F,  0.0F,  0.0F, (float) Math.random() * 2.0F);
                             }
                         }
 
