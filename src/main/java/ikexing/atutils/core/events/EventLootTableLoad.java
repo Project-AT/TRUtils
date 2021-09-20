afb9f8f767f0409113383b39e6a8c94784e27c9f
Raylras <raylras@outlook.com>
2021-09-20 16:47:15 +0800

    Revert "fix(EventLootTableLoad): 修复删除entry导致的异常"
    This reverts commit b6e3f2eb798ac723295a3cf88008936060dc306c.

--- src/main/java/ikexing/atutils/core/events/EventLootTableLoad.java
+++ src/main/java/ikexing/atutils/core/events/EventLootTableLoad.java
@@ -12,7 +12,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 // REFLECTION HELL
@@ -20,7 +19,7 @@ import java.util.List;
 @Mod.EventBusSubscriber(modid = "atutils")
 public class EventLootTableLoad {
 
-    @SubscribeEvent(priority = EventPriority.LOWEST)
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onLootTableLoad(LootTableLoadEvent event) {
         LootTableManager manager = event.getLootTableManager();
         LootTable table = event.getTable();
@@ -31,8 +30,7 @@ public class EventLootTableLoad {
             ATUtils.logger.debug("Skipped modifying loot table {} because it is frozen", event.getName());
         } else {
             Field field = ReflectUtil.getField(table.getClass(), "pools"); // try MCP name
-            if (field == null)
-                field = ReflectUtil.getField(table.getClass(), "field_186466_c"); // if null, try SRG name
+            if (field == null) field = ReflectUtil.getField(table.getClass(), "field_186466_c"); // if null, try SRG name
             if (field == null) {
                 ATUtils.logger.debug("Reflection error: {}", event.getName()); // Oops
                 return;
@@ -59,8 +57,7 @@ public class EventLootTableLoad {
                 ATUtils.logger.debug("Skipped modifying loot pool {} because it is frozen", pool.getName());
             } else {
                 Field field = ReflectUtil.getField(pool.getClass(), "lootEntries"); // try MCP name
-                if (field == null)
-                    field = ReflectUtil.getField(table.getClass(), "field_186453_a"); // if null, try SRG name
+                if (field == null) field = ReflectUtil.getField(table.getClass(), "field_186453_a"); // if null, try SRG name
                 if (field == null) {
                     ATUtils.logger.debug("Reflection error: {}", pool.getName()); // Oops
                     return;
@@ -73,35 +70,20 @@ public class EventLootTableLoad {
                 }
             }
 
-            Iterator<LootEntry> it = lootEntries.iterator();
-
-            while (it.hasNext()) {
-                LootEntry entry = it.next();
-
-                // Remove non-vanilla items.
+            for (LootEntry entry : lootEntries) {
                 if (entry instanceof LootEntryItem) {
-                    LootEntryItem entryItem = (LootEntryItem) entry;
+                    LootEntryItem entryItem = (LootEntryItem)entry;
                     Item item = entryItem.item;
 
-
-                    if (item.getRegistryName().getNamespace() != "minecraft") {
-                        it.remove();
-                        ATUtils.logger.info("Removed entry item({}) from pool({}/{})", item.getRegistryName(), event.getName(), pool.getName());
+                    // Remove non-vanilla items.
+                    if (!item.getRegistryName().getNamespace().startsWith("minecraft")) {
+                        pool.removeEntry(entry.getEntryName());
+                        ATUtils.logger.info("Removed item({}) from pool({}/{})", item.getRegistryName(), event.getName(), pool.getName());
                     }
 
                 }
-
-                // Remove non-vanilla items.
-                if (entry instanceof LootEntryTable) {
-                    LootEntryTable entryTable = (LootEntryTable) entry;
-                    if (entryTable.table.getPath().contains("inject")) {
-                        it.remove();
-                        ATUtils.logger.info("Removed entry table({}) from pool({}/{})", entryTable.table.toString(), event.getName(), pool.getName());
-                    }
-
-                }
-
             }
+
         }
 
 
