86a1f548f5d8fa7673549e594257d448029df8c0
ikexing <ikexing@qq.com>
2021-11-21 12:43:45 +0800

    fix name

--- src/main/java/ikexing/atutils/ATUtils.java
+++ src/main/java/ikexing/atutils/ATUtils.java
@@ -46,7 +46,7 @@ public class ATUtils {
     public static final String VERSION = "1.1.5";
     public static final String dependencies = "required-after:crafttweaker;after:contenttweaker;required-after:mixinbooter;after:twilightforest;after:botania;before:mana_craft";
 
-    public static final List<String> CANCEL_ORES = Lists.newArrayList("ingot", "Glass", "nugget", "dust", "cocl", "gem", "stone", "ore");
+    public static final List<String> CANCEL_ORES = Lists.newArrayList("ingot", "Glass", "nugget", "dust", "coal", "gem", "stone", "ore");
 
     public static RitualBase ritualMa;
     public static Block circuitry;
