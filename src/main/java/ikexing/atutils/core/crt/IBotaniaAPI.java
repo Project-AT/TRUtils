e1650da1e8026b79ad97bd3e1321036f92280d31
ikexing <ikexing@qq.com>
2022-04-07 20:22:31 +0800

    添加botania精灵门配方删除

--- src/main/java/ikexing/atutils/core/crt/IBotaniaAPI.java
+++ src/main/java/ikexing/atutils/core/crt/IBotaniaAPI.java
@@ -1,22 +1,36 @@
 package ikexing.atutils.core.crt;
 
 import com.blamejared.mtlib.helpers.InputHelper;
+import com.blamejared.mtlib.helpers.StackHelper;
 import crafttweaker.CraftTweakerAPI;
 import crafttweaker.IAction;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.item.IIngredient;
 import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
 import ikexing.atutils.ATUtils;
 import ikexing.atutils.core.goodfeeling.IGoodFeeling;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
+import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.recipe.RecipeElvenTrade;
-
-import java.util.Map;
 
 @ZenRegister
 @ZenClass("mods.atutils.IBotaniaAPI")
 public class IBotaniaAPI {
+
+    @ZenMethod
+    public static void removeElvenTradeRecipe(IIngredient output) {
+        CraftTweakerAPI.apply(new IAction() {
+            @Override public void apply() {
+                BotaniaAPI.elvenTradeRecipes.removeIf(recipe -> StackHelper.matches(output, CraftTweakerMC.getIItemStacks(recipe.getOutputs())));
+            }
+
+            @Override public String describe() {
+                return "Removing ElvenTrade Recipe -> " + output.toCommandString();
+            }
+        });
+    }
 
     @ZenMethod
     public static void addElvenTradeRecipe(String recipeName, int level, double experience, IIngredient[] input, IItemStack... outputs) {
