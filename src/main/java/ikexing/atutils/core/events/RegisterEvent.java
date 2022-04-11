c7fc0dfd7a37d09a569cfaebcac5addb8a3dcca8
ikexing <ikexing@qq.com>
2022-04-11 15:43:22 +0800

    修复药水注册问题

--- src/main/java/ikexing/atutils/core/events/RegisterEvent.java
+++ src/main/java/ikexing/atutils/core/events/RegisterEvent.java
@@ -33,14 +33,6 @@ import net.minecraftforge.registries.IForgeRegistry;
 @EventBusSubscriber
 public class RegisterEvent {
 
-    public static final Potion POTION_DROWSY = new PotionDrowsy();
-    public static final PotionType POTION_TYPE_DROWSY =
-            new PotionType(ATUtils.MODID + ".drowsy", new PotionEffect(POTION_DROWSY, 3600)).setRegistryName("drowsy");
-    public static final PotionType POTION_TYPE_LONG_DROWSY =
-            new PotionType(ATUtils.MODID + ".drowsy", new PotionEffect(POTION_DROWSY, 9600)).setRegistryName("long_drowsy");
-    public static final PotionType POTION_STRONG_TYPE_DROWSY =
-            new PotionType(ATUtils.MODID + ".drowsy", new PotionEffect(POTION_DROWSY, 3600)).setRegistryName("strong_drowsy");
-
     @SubscribeEvent
     public static void onItemRegistry(Register<Item> event) {
         IForgeRegistry<Item> registry = event.getRegistry();
@@ -68,15 +60,15 @@ public class RegisterEvent {
     @SubscribeEvent
     public static void onPotionRegistry(RegistryEvent.Register<Potion> event) {
         IForgeRegistry<Potion> registry = event.getRegistry();
-        registry.register(POTION_DROWSY);
+        registry.register(PotionDrowsy.INSTANCE);
     }
 
     @SubscribeEvent
     public static void onPotionTypeRegistry(RegistryEvent.Register<PotionType> event) {
         IForgeRegistry<PotionType> registry = event.getRegistry();
-        registry.register(POTION_TYPE_DROWSY);
-        registry.register(POTION_TYPE_LONG_DROWSY);
-        registry.register(POTION_STRONG_TYPE_DROWSY);
+        registry.register(PotionDrowsy.POTION_TYPE_DROWSY);
+        registry.register(PotionDrowsy.POTION_TYPE_LONG_DROWSY);
+        registry.register(PotionDrowsy.POTION_STRONG_TYPE_DROWSY);
     }
 
     @SubscribeEvent
--- src/main/java/ikexing/atutils/core/potions/PotionDrowsy.java
+++ src/main/java/ikexing/atutils/core/potions/PotionDrowsy.java
@@ -4,11 +4,23 @@ import ikexing.atutils.ATUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionType;
 import net.minecraft.util.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
+@SuppressWarnings("deprecation")
 public class PotionDrowsy extends Potion {
+
+    public static final PotionDrowsy INSTANCE = new PotionDrowsy();
+
+    public static final PotionType POTION_TYPE_DROWSY =
+            new PotionType(ATUtils.MODID + ".drowsy", new PotionEffect(INSTANCE, 3600)).setRegistryName("drowsy");
+    public static final PotionType POTION_TYPE_LONG_DROWSY =
+            new PotionType(ATUtils.MODID + ".drowsy", new PotionEffect(INSTANCE, 9600)).setRegistryName("long_drowsy");
+    public static final PotionType POTION_STRONG_TYPE_DROWSY =
+            new PotionType(ATUtils.MODID + ".drowsy", new PotionEffect(INSTANCE, 3600)).setRegistryName("strong_drowsy");
 
     private static final ResourceLocation TEXTURE =
             new ResourceLocation(ATUtils.MODID + ":textures/gui/drowsy.png");
@@ -30,7 +42,7 @@ public class PotionDrowsy extends Potion {
     }
 
     @Override
-    public void renderHUDEffect(int x, int y, PotionEffect effect, Minecraft mc, float alpha) {
+    public void renderHUDEffect(int x, int y, @NotNull PotionEffect effect, Minecraft mc, float alpha) {
         mc.getTextureManager().bindTexture(TEXTURE);
         mc.ingameGUI.drawTexturedModalRect(
                 x + 3, y + 3, 0, 0, 18, 18);
